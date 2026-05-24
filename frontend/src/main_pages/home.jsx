62fde9189538d8607d0125cc9fcc5b3b666ddf3c
Sanithu <SanithuHasmalKudaliyana@gmail.com>
2026-05-24 13:50:14 +0530

    updating home.jsx
    Fix HTML tags in home page description

--- frontend/src/main_pages/home.jsx
+++ frontend/src/main_pages/home.jsx
@@ -17,12 +17,12 @@ export default function Home() {
         <>
             <div className="home-container">
                 <h1>Welcome to Home</h1>
-                <p2>A Hospital Information System (HIS) is a comprehensive, computer-based system used in hospitals to manage and organize all aspects of healthcare operations.
+                <p>A Hospital Information System (HIS) is a comprehensive, computer-based system used in hospitals to manage and organize all aspects of healthcare operations.
                     It integrates clinical, administrative, and financial data to ensure smooth coordination between different departments such as outpatient services, inpatient care,
                     laboratory, pharmacy, and billing. By digitizing patient records and workflows, HIS allows healthcare professionals to quickly access accurate medical histories,
                     test results, and treatment plans, which improves decision-making and reduces medical errors. It also helps hospital staff streamline tasks like appointment scheduling,
                     resource allocation, and inventory management. Overall, a Hospital Information System enhances efficiency, improves patient care quality, and supports better communication
-                    the entire healthcare facility.</p2>
+                    the entire healthcare facility.</p>
             </div>
             <div className="reviews">
                 <div className="review-card">
