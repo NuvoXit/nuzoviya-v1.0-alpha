9f461671eacd62664e9cea8632ab95c251439f41
Maatheyshan <gowreesanmaatheyshan.org@gmail.com>
2026-06-23 22:54:55 +0530

    pushing to a private repo with only patient and doctor

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,8 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from config import Database
-from models import Patient, Booking, Login, Doctor, Nurse, Radiologist, MLT, Optician
+from models import Patient, Booking, Login, Doctor, Nurse
+# from models import Radiologist, MLT, Optician
 
 
 admin = Admin(name="Medical Admin Panel")
@@ -15,6 +16,6 @@ def init_admin(app):
     admin.add_view(ModelView(Login, Database.session))
     admin.add_view(ModelView(Doctor, Database.session, category="Staff"))
     admin.add_view(ModelView(Nurse, Database.session, category="Staff"))
-    admin.add_view(ModelView(Radiologist, Database.session, category="Staff"))
-    admin.add_view(ModelView(MLT, Database.session, category="Staff"))
-    admin.add_view(ModelView(Optician, Database.session, category="Staff"))
+    # admin.add_view(ModelView(Radiologist, Database.session, category="Staff"))
+    # admin.add_view(ModelView(MLT, Database.session, category="Staff"))
+    # admin.add_view(ModelView(Optician, Database.session, category="Staff"))
